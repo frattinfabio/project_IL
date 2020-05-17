bcc193499e981f6cffcf83713538ee27bc6c2ea0
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-17 11:48:40 +0200

    getting the splitting faster

--- incremental_dataset.py
+++ incremental_dataset.py
@@ -1,26 +1,30 @@
 from torchvision.datasets import CIFAR100
 from torch.utils.data import Subset
-import pandas as pd
-import random
+import random 
 
 # splitting the 100 classes in [num_groups] groups
 # and the indexes of the images belonging to those classes as well
-def split_classes(dataset, num_groups):
-  assert 100 % num_groups == 0
+def split_labels(dataset, num_groups):
 
-  classes = list(range(100))
-  classes_per_group = 100 / num_groups
-  classes_split = []
-  indexes_split = []
+  labels = list(range(100))
+  labels_per_group = 100 // num_groups
+  labels_split = []
+  indexes_split = [[] for _ in range(num_groups)]
 
   for _ in range(num_groups):
     random.seed(42)
-    subset = random.sample(classes, classes_per_group)
-    classes_split.append(subset)
-    indexes = [idx for idx in range(len(dataset)) if dataset.__getitem__(idx)[1] in subset]
-    indexes_split.append(indexes)
-    classes = list(set(classes) - set(subset))
-  return classes_split, indexes_split
+    subset = random.sample(labels, labels_per_group)
+    labels_split.append(subset)
+    labels = list(set(labels) - set(subset))
+
+  for idx in range(len(dataset)):
+    label = dataset.__getitem__(idx)[1]
+    for i in range(num_groups):
+      if label in labels_split[i]:
+        indexes_split[i].append(idx)
+        break
+      
+  return labels_split, indexes_split
 
 
 
@@ -32,15 +36,9 @@ class IncrementalCIFAR():
         
         self.dataset = CIFAR100(root, train=train, transform=transform, target_transform=target_transform, download=download)
         self.num_groups = num_groups
-        self.classes_split, self.indexes_split = split_classes(num_groups)
+        self.labels_split, self.indexes_split = split_labels(self.dataset, num_groups)
 
-  def __getitem__(self, index):
-    return self.dataset.__getitem__(index)
-
-  def __len__(self):
-    return self.dataset.__len__()
-
-# get the subset of the dataset relative to the [group_index] group
+  # get the subset of the dataset relative to the [group_index] group
   def get_group(self, group_index):
     indexes = self.indexes_split[group_index]
     return Subset(self.dataset, indexes)
