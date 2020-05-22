4f0c54b77ac56654fc2576baa80b51d1bc4359b3
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-22 14:32:05 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import torch 
 
-def transforms_labels_onehot(labels, num_labels):
+def transform_labels_onehot(labels, num_labels):
   onehot = torch.zeros((len(labels), num_labels))
   for i in range(len(labels)):
     onehot[i][labels[i]] = 1
