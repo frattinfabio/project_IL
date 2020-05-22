1e9f1f7cd1341ed1f3b6f82fb7abb9e73e166902
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-22 10:24:44 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 import torch 
 
 def transforms_labels_onehot(labels, num_labels):
-  transformed = []
+  onehot = torch.zeros((len(labels), num_labels))
   for i in range(len(labels)):
-    onehot = []
-    for label in range(num_labels):
-      onehot.append(1 if labels[i] == label else 0)
-    transformed.append(onehot)
-  return torch.Tensor(transformed)
+    onehot[i][labels[i]] = 1
+  return onehot
