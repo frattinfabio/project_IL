c85f9598eeb42cf4d0ab4bcd010b0969ea3c487a
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-22 11:35:18 +0200

    Update multi_class_cross_entropy_loss.py

--- multi_class_cross_entropy_loss.py
+++ multi_class_cross_entropy_loss.py
@@ -1,23 +1,31 @@
-from torch.nn.modules.loss import _WeightedLoss
+import torch.nn as nn
 
 # function MultiClassCrossEntropy taken from the GitHub repo https://github.com/ngailapdi/LWF
 
-class MultiClassCrossEntropyLoss(_WeightedLoss):
+class MultiClassCrossEntropyLoss(nn.Module):
 
-  def __init__(self, weight=None, size_average=None, ignore_index=-100,
-                 reduce=None, reduction='mean', T = 1):
-        super(MultiClassCrossEntropyLoss, self).__init__(weight, size_average, reduce, reduction)
-        self.T = T
+  def __init__(self, reduction='mean', T = 1):
+    super(MultiClassCrossEntropyLoss, self).__init__()
+    self.reduction = reduction
+    self.T = T
         
-  def MultiClassCrossEntropy(logits, labels, T):
-    labels = Variable(labels.data, requires_grad=False).cuda()
-    outputs = torch.log_softmax(logits/T, dim=1)   # compute the log of softmax values
-    labels = torch.softmax(labels/T, dim=1)
-    outputs = torch.sum(outputs * labels, dim=1, keepdim=False)
-    outputs = -torch.mean(outputs, dim=0, keepdim=False)
-    return Variable(outputs.data, requires_grad=True).cuda()
+  def MultiClassCrossEntropy(output, target, T):
+    output = output.narrow(1, 0, target.shape[1])
     
-  def forward(self, input, target):
+    output = torch.log_softmax(output, dim=1)
+    target = torch.softmax(target * self.alpha, dim=1)
+    loss = (output * target).mean(dim=1)
+
+    if self.reduction == 'mean':
+        loss = -torch.mean(loss)
+    elif self.reduction == 'sum':
+        loss = -torch.sum(loss)
+    else:
+        loss = -loss
+
+    return loss
+
+def forward(self, input, target):
     return MultiClassCrossEntropy(input, targets, self.T)
   
     
