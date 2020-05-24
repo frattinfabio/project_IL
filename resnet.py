4a006d4a82c9d5bfa93ab681147b0f19462bba6c
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-24 16:08:42 +0200

    added feature_extraction

--- resnet.py
+++ resnet.py
@@ -106,18 +106,20 @@ class ResNet(nn.Module):
             self.in_planes = planes * block.expansion
 
         return nn.Sequential(*layers)
-
-    def forward(self, x):
+    
+    def feature_extraction(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = F.avg_pool2d(out, out.size()[3])
         out = out.view(out.size(0), -1)
-        out = self.linear(out)
         return out
 
-
+    def forward(self, x):
+        out = self.linear(self.feature_extraction(x))
+        return out
+    
 def resnet20():
     return ResNet(BasicBlock, [3, 3, 3])
 
