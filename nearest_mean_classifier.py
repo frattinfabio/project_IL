483d29c9ab60cb98c7e2e7ea5594eff505ab4984
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-25 16:54:22 +0200

    Create nearest_mean_classifier.py

---
+++ nearest_mean_classifier.py
@@ -0,0 +1,30 @@
+import torch
+
+class NearestMeanOfExamplarsClassifier():
+  def __init__(self, feature_extractor, feat_dim, examplars):
+    self.feature_extractor = feature_extractor
+    self.means = []
+
+    for i in range(len(examplars)):
+      examplar_set = examplars[i]
+      features_mean = torch.zeros((feat_dim,))
+      for j in range(len(examplar_set)):
+        tensor = transforms.ToTensor()(examplar_set[j]).unsqueeze(0).cuda()
+        features = sefl.feature_extractor(tensor).squeeze(0).cpu()
+        features = features / torch.norm(features, p = 2)
+        features_mean += features
+      features_mean /= len(examplar_set)
+      features_mean = features_mean / torch.norm(features_mean, p = 2)
+      self.means.append(features_mean)
+      self.means = torch.stack(self.means)
+      
+        
+  def classify(self, input_images):
+    features = self.feature_extractor(input_images.cuda())
+    features = features / torch.norm(features, p = 2)
+    preds = []
+    for feat in feats:
+      distances = torch.pow(self.means.cuda() - feat, 2).sum(-1)
+      preds.append(distances.argmin().item())
+    return np.array(preds)
+
