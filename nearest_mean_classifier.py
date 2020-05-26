f130655950bdbd14218f24c945602fcda7f53976
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-26 11:41:47 +0200

    Update nearest_mean_classifier.py

--- nearest_mean_classifier.py
+++ nearest_mean_classifier.py
@@ -4,34 +4,35 @@ from torchvision import transforms
 
 class NearestMeanOfExamplarsClassifier():
   def __init__(self, net, examplars, transform):
-    self.net = net
-    self.net.train(False)
+    with torch.no_grad():
+      self.net = net
+      self.net.train(False)
 
-    self.means = []
+      self.means = []
+      for i in range(len(examplars)):
+        examplar_set = examplars[i][:,0]
+        features_mean = torch.zeros((net.linear.in_features,))
 
-    for i in range(len(examplars)):
-      examplar_set = examplars[i][:,0]
-      features_mean = torch.zeros((net.linear.in_features,))
-      
-      for j in range(len(examplar_set)):
-        tensor = transform(examplar_set[j]).unsqueeze(0).cuda()
-        features = self.net.features_extraction(tensor).squeeze(0).cpu()
-        features = features / torch.norm(features, p = 2)
-        features_mean += features
-      features_mean /= len(examplar_set)
-      features_mean = features_mean / torch.norm(features_mean, p = 2)
-      self.means.append(features_mean)
-      
-    self.means = torch.stack(self.means)
+        for j in range(len(examplar_set)):
+          tensor = transform(examplar_set[j]).unsqueeze(0).cuda()
+          features = self.net.features_extraction(tensor).squeeze(0).cpu()
+          features = features / torch.norm(features, p = 2)
+          features_mean += features
+        features_mean /= len(examplar_set)
+        features_mean = features_mean / torch.norm(features_mean, p = 2)
+        self.means.append(features_mean)
+
+      self.means = torch.stack(self.means)
       
         
   def classify(self, input_images):
-    self.net.train(False)
-    features = self.net.features_extraction(input_images)
-    features = features / torch.norm(features, p = 2)
-    preds = []
-    for feature in features:
-      distances = torch.pow(self.means.cuda() - feature, 2).sum(-1)
-      preds.append(distances.argmin().item())
-    return torch.Tensor(preds).cuda()
+    with torch.no_grad():
+      self.net.train(False)
+      features = self.net.features_extraction(input_images)
+      features = features / torch.norm(features, p = 2)
+      preds = []
+      for feature in features:
+        distances = torch.pow(self.means.cuda() - feature, 2).sum(-1)
+        preds.append(distances.argmin().item())
+     return torch.Tensor(preds).cuda()
 
