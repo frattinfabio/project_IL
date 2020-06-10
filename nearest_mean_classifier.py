9c9148cde6e0897f56db62bfbc055aa62baeb753
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-06-10 19:52:01 +0200

    Update nearest_mean_classifier.py

--- nearest_mean_classifier.py
+++ nearest_mean_classifier.py
@@ -9,36 +9,34 @@ from torchvision import transforms
 # [train_dataset]: the SubCIFAR object containing the data of the new classes
 # [transform]: transform image into tensor and normalize
 class NearestMeanOfExamplarsClassifier():
-  def __init__(self, net, examplars, train_dataset, transform):
-    with torch.no_grad():
+  def __init__(self):
+      self.net = None
+      self.means = None
+      
+  def update(self, step, net, train_dataloader):
       self.net = net
-      self.net.train(False)
-      self.means = []
-      for i in range(len(examplars) + len(train_dataset.stored_labels)):
-        # for the new classes, consider as examplar set all the available training
-        if i >= len(examplars):
-          mapped_label = train_dataset.all_labels[i]
-          label_mask = (train_dataset.dataFrame["label"] == mapped_label)
-          examplar_set = train_dataset.dataFrame[label_mask]["image"].values
-        else:
-          examplar_set = examplars[i][:,0]
-          
-        features_mean = torch.zeros((net.fc.in_features,))
+      n_known_classes = self.net.fc.out_features
+      self.means = [torch.zeros((self.net.fc.in_features,)) for _ in range(n_known_classes)]
+      num_images = torch.zeros(n_known_classes,)
 
-        for j in range(len(examplar_set)):
-          tensor = transform(examplar_set[j]).unsqueeze(0).cuda()
+      with torch.no_grad():
+        self.net = self.net.cuda()
+        self.net = self.net.train(False)
 
-          features = self.net(tensor, output = 'features')
-          features = features.squeeze(0).cpu()
-          features = features / torch.norm(features, p = 2)
-          
-          features_mean += features
-        features_mean /= len(examplar_set)
-        features_mean = features_mean / torch.norm(features_mean, p = 2)
-        self.means.append(features_mean)
+        for images, labels in train_dataloader:
+          images = images.cuda()
+          labels = labels.cuda()
+          features = net(images, output = 'features')
+          features = F.normalize(features, p = 2)
+          for feature, label in zip(features, labels):
+            self.means[label] += feature
+            num_images[label] += 1
+        for label in range(n_known_classes):
+          self.means[label] /= num_images[label]
 
       self.means = torch.stack(self.means)
-      
+      self.means = F.normalize(means, p = 2)
+
   # predict the labels for the batch [input_images]
   # according to the nearest mean criterion 
   def classify(self, input_images):
