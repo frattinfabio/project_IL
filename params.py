79497e6cfc593bd6b8a70e1432265156474efe2f
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-06-15 11:54:44 +0200

    Add files via upload

---
+++ params.py
@@ -0,0 +1,64 @@
+train_params_base = {
+"LR": 2 ,
+"MOMENTUM": 0.9 ,
+"WEIGHT_DECAY": 1e-5,
+"STEP_MILESTONES": [49,63],
+"GAMMA": 0.2,
+"NUM_EPOCHS": 70,
+"train_transform": transforms.Compose([
+                                      transforms.RandomCrop(32, padding = 4),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                      ]),
+"test_transform": transforms.Compose([
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                     ]),
+}
+
+approach_params_finetuning = {
+"classification_loss": "bce",
+"distillation_loss": None,
+"use_distillation" : False,
+"use_variation" : False,
+"use_exemplars": False,
+"n_exemplars": 0
+}
+
+approach_params_lwf = {
+"classification_loss": "bce",
+"distillation_loss": "icarl",
+"use_distillation" : False,
+"use_variation" : False,
+"use_exemplars": False,
+"n_exemplars": 0
+}
+
+approach_params_icarl = {
+"classification_loss": "bce",
+"distillation_loss": "icarl",
+"use_distillation" : True,
+"use_variation" : False,
+"use_exemplars": True,
+"n_exemplars": 2000
+}
+
+approach_params_variation = {
+"classification_loss": "bce",
+"distillation_loss": "icarl",
+"use_distillation" : True,
+"use_variation" : True,
+"use_exemplars": True,
+"n_exemplars": 2000
+}
+
+def get_params(method):
+    if method == "FINETUNING":
+        return train_parms_base, approach_params_finetuning
+    elif method == "LWF":
+        return train_parms_base, approach_params_lwf
+    elif method == "ICARL":
+        return train_parmas_base, approach_params_icarl
+    else:
+        return train_parms_base, approach_params_variation
