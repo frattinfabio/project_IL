53c74d85f7b5cbc0c1f2255c24a6a408958a0957
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-06-18 16:27:16 +0200

    comments added

--- data_handler/LabelsSplitter.py
+++ data_handler/LabelsSplitter.py
@@ -1,11 +1,13 @@
 import random 
-# LabelsSplitter class
+
+# [LabelsSplitter] class
 # define the labels splitting, given the total number of labels and the number of groups
+# [num_labels]: the total number of classes to be split
+# [num_groups]: the number of groups to create, each of which will contain num_label//num_groups labels
 # WARNING: it is needed that num_labels % num_groups == 0
 
 class LabelsSplitter():
   def __init__(self, num_labels, num_groups, seed = 42):
-    assert num_labels % num_groups == 0
 
     self.labels_split = []
     labels = list(range(num_labels))
