d55741dc417b8ed50c52c800d6a48657c47655b5
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-06-18 16:24:23 +0200

    comments added

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch 
 
+# transform the [labels] array into a one_hot encoding array
 def transform_labels_onehot(labels, num_labels):
   onehot = torch.zeros((len(labels), num_labels))
   for i in range(len(labels)):
